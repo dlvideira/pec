9490aa533685662fe14f7a18741937ce598e97fe
Daniel Lisboa <daniel.videira@protonmail.com>
2020-06-18 22:43:06 -0300

    configuring mongo and updating application-properties

--- src/main/java/com/pec/personalexpensescontrol/PecApplication.java
+++ src/main/java/com/pec/personalexpensescontrol/PecApplication.java
@@ -2,12 +2,16 @@ package com.pec.personalexpensescontrol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PecApplication {
+public class PecApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PecApplication.class, args);
+	public static void main(String[] args) {SpringApplication.run(PecApplication.class, args);}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PecApplication.class);
 	}
-
 }
