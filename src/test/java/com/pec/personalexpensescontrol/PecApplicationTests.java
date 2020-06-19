abff94cde1b570ecd1214c320cccc48d60d424bd
Daniel Lisboa <daniel.videira@protonmail.com>
2020-06-18 21:33:38 -0300

    first commit

---
+++ src/test/java/com/pec/personalexpensescontrol/PecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pec.personalexpensescontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
