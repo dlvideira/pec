0ec7c7b8757070ece8494ed278140aa348a6e83c
Daniel Lisboa <daniel.videira@protonmail.com>
2020-07-04 23:19:45 -0300

    implementing methods in userController

--- src/main/java/com/pec/personalexpensescontrol/controller/ExpenseController.java
+++ src/main/java/com/pec/personalexpensescontrol/controller/ExpenseController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class AccountController {
+public class ExpenseController {
     @Autowired
     private ExpenseService expenseService;
 
--- src/main/java/com/pec/personalexpensescontrol/repository/UserManagementRepository.java
+++ src/main/java/com/pec/personalexpensescontrol/repository/UserManagementRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface UserManagementRepository extends MongoRepository<User, ObjectId> {
     Optional<User> findByEmail(String email);
+
+    Optional<User> findById(String userId);
 }
