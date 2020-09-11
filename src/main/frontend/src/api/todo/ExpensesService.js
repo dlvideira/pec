fa0054b67991f1b3c92ebc881fb820b6ddff90cb
Daniel Lisboa <daniel.videira@protonmail.com>
2020-09-11 00:50:41 -0300

    implementing updateExpense page / WIP createExpense

--- src/main/frontend/src/api/todo/ExpensesService.js
+++ src/main/frontend/src/api/todo/ExpensesService.js
@@ -10,5 +10,17 @@ class ExpensesService {
     deleteExpense(expenseId) {
         return axios.delete(`http://localhost:8080/expenses/${this.userId}/deleteExpense?expenseId=${expenseId}`)
     }
+
+    getExpense(expenseId) {
+        return axios.get(`http://localhost:8080/expenses/${this.userId}/getExpense?expenseId=${expenseId}`)
+    }
+
+    updateExpense(expense) {
+        return axios.patch(`http://localhost:8080/expenses/${this.userId}/updateExpense`, expense)
+    }
+
+    createExpense(expense) {
+        return axios.post(`http://localhost:8080/expenses/${this.userId}/createExpense`, expense)
+    }
 }
 export default new ExpensesService()
--- src/main/frontend/src/components/todo/TodoComponent.jsx
+++ src/main/frontend/src/components/todo/TodoComponent.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import {Field, Form, Formik} from "formik";
+import ExpensesService from "../../api/todo/ExpensesService.js";
 
 
 class TodoComponent extends Component {
@@ -7,29 +8,72 @@ class TodoComponent extends Component {
         super(props)
 
         this.state = {
-            expenseId : this.props.match.params.id,
-            expenseName : "hardCoded",
-            amount : 0,
-            category : "hardCoded",
-            totalParcels : 0,
-            currentParcel : 0,
-            frequency : "fazer Lista Suspensa",
+            expenseId: this.props.match.params.id,
+            expenseName: "",
+            amount: 0,
+            category: "",
+            totalParcels: "",
+            currentParcel: 0,
+            frequency: 0
         }
         this.onSubmit = this.onSubmit.bind(this)
     }
 
     onSubmit(values){
-        console.log(values)
+        let expenseToUpdate = {
+            expenseId: this.state.expenseId,
+            expenseName: values.expenseName,
+            amount: values.amount,
+            category: values.category,
+            totalParcels: values.totalParcels,
+            currentParcel: values.currentParcel,
+            frequency: values.frequency
+        }
+        if (this.state.id === 0){
+            ExpensesService.createExpense(expenseToUpdate)
+                .then(() => this.props.history.push('/todos'))
+        }else {
+            ExpensesService.updateExpense(expenseToUpdate)
+                .then(() => this.props.history.push('/todos'))
+        }
+    }
+
+    componentDidMount() {
+        if(this.state.expenseId=== 0) {
+            return
+        }
+        this.refreshExpense();
+    }
+
+    refreshExpense() {
+        if(this.state.expenseId=== 0) {
+            return
+        }
+        ExpensesService.getExpense(this.state.expenseId)
+            .then(
+                response => {
+                    this.setState({
+                        expenseId: response.data.expenseId,
+                        expenseName: response.data.expenseName,
+                        amount: response.data.amount,
+                        category: response.data.category,
+                        totalParcels: response.data.totalParcels,
+                        currentParcel: response.data.currentParcel,
+                        frequency: response.data.frequency
+                    })
+                }
+            )
     }
 
     render() {
-        let {expenseName, amount, category, totalParcels, currentParcel, frequency} = this.state
+        let {expenseId, expenseName, amount, category, totalParcels, currentParcel, frequency} = this.state
         return (
             <div>
                 <h1>Todo</h1>
                     <div className="container">
                         <Formik
-                            initialValues={{expenseName, amount, category, totalParcels, currentParcel, frequency}}
+                            initialValues={{expenseId, expenseName, amount, category, totalParcels, currentParcel, frequency}}
+                            enableReinitialize={true}
                             onSubmit={this.onSubmit}>
                             {
                                 (props) => (
@@ -55,7 +99,7 @@ class TodoComponent extends Component {
                                             <Field className="form-control" type="number" name="currentParcel"/>
                                         </fieldset>
                                         <fieldset className="form-group">
-                                            <label>Frequência</label>
+                                            <label>Frequência (dias)</label>
                                             <Field className="form-control" type="text" name="frequency"/>
                                         </fieldset>
                                         <button className="btn btn-success" type="submit">Salvar</button>
